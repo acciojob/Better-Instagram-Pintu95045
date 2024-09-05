07083d4410cc22138d3bf0a27810f417b8c58af9
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-05 23:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 //your code here
-document.addEventListener("DOMContentLoaded",()=>{
-let draggables = document.querySelectorAll(".image")
-// console.log(draggables)
-
-let draggedElement = null // which card/div you have started dragging
-draggables.forEach(draggable =>{
-
-    // dragStart, dragEnd
-
-    draggable.addEventListener("dragstart",(e)=>{
-         draggedElement = e.target
-        //  e.dataTransfer.setData("text", e.target.id)
-         draggedElement.style.opacity = 0.5
-    })
-
-    draggable.addEventListener("dragend",(e)=>{
-        e.target.style.opacity = 1
-   })
-
-   // dragover, dragenter, drop
-   let dragEvents = ["dragover", "dragenter", "drop"]
-//    draggable.addEventListener("dragover", (e)=>{
-//         e.preventDefault()
-//    })
-
-    dragEvents.forEach(drag =>{
-        draggable.addEventListener(drag, (e)=>{
-                e.preventDefault()
-
-            if(drag == "drop"){
-                 const targetElement = e.target // where you want to drop it
-                console.log("Helllloo")
-                if(targetElement != draggedElement){
-                    // swap the background image
-                    const draggedBackground = draggedElement.id
-                    draggedElement.id = targetElement.id
-                    targetElement.id = draggedBackground
-
-                    // swap: 
-
-                    const draggedText = draggedElement.innerText
-                    draggedElement.innerText = targetElement.innerText
-                    targetElement.innerText = draggedText
-
-
-                }
-
-
-
-            }
-        })
-    })
-
-
-})
-})
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
+}
+ 
+function allowDrop(e) {
+  e.preventDefault();
+}
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
+}
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
